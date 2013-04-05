a466d919bc7c6b8dd89f06af551628169f02abe5
Sebestyen <r0bi3000@lyahoo.com>
2013-04-05 09:40:19 +0300

    Uplod dir added

--- views/js/minicpromo.js
+++ views/js/minicpromo.js
@@ -10,9 +10,5 @@ jQuery(document).ready(function($) {
 	$('.description').each(function() {
         $('#'+$(this).attr('id')).wysihtml5();
     });
-    if($('#accordion2 .accordion-toggle').hasClass('collapsed')){
-    	$(this).addClass('close');
-    }else{
-    	$(this).addClass('open');
-    }
+
 });
